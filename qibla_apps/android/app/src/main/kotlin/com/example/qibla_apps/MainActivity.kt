c9de073d1d05115dd25df3190824c6152306018c
SyedShahzainHussain <suedshahzainhussain001@gmail.com>
2024-02-10 21:26:15 +0500

    add splash screen

---
+++ qibla_apps/android/app/src/main/kotlin/com/example/qibla_apps/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.qibla_apps
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ qibla_apps/lib/screen/splash_screen.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => SplashScreenState();
+}
+
+class SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(context) {
+    return Scaffold(
+      body: Column(children: [
+        Center(
+          child: Padding(
+            padding: EdgeInsets.only(
+              top: MediaQuery.sizeOf(context).height / 5,
+            ),
+            child: Image.asset(
+              "assets/images/qibla.png",
+              color: Colors.blueGrey,
+              width: MediaQuery.sizeOf(context).width * .8,
+            ),
+          ),
+        ),
+        SizedBox(
+          height: MediaQuery.sizeOf(context).height * 0.05,
+        ),
+        Text(
+          "Welcome To Qibla Finder App",
+          style: Theme.of(context).textTheme.headlineSmall!.copyWith(
+                color: Colors.black,
+                fontStyle: FontStyle.italic,
+            fontWeight: FontWeight.bold,
+              ),
+        )
+      ]),
+    );
+  }
+}
